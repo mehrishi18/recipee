a4dbd6e0c0b514b4c81e9e14189a841be46b9edc
mehrishi18 <sahrishikesh8@gmail.com>
2022-07-02 16:19:26 +0530

    recipee created

---
+++ src/app/addrecipee/addrecipee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addrecipee',
+  templateUrl: './addrecipee.component.html',
+  styleUrls: ['./addrecipee.component.css']
+})
+export class AddrecipeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
